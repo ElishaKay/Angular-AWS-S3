75f1e95c47029f7aee75bc5078c79c58e4500f19
elishakay <kramer1346@gmail.com>
2018-01-21 00:37:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# AngularJS Client Side Image Compression and Upload to AWS S3
+An example AngularJS app demonstrating client-side image compression and uploading to AWS S3.
+
+## Demonstrates: ##
+* AWS S3 Integration
+* HTML5 Canvas
+* HTML5 FileReader API
+* AngularJS
+* Angular Bootstrap (Tabs)
+* Array Manipulation
+* Bootstrap CSS
+* Basic Responsive Design
+* Basic Form Validation
+
+## Credits: ##
+* [resample_hermite](https://github.com/viliusle/Hermite-resize/)
+* [dataURItoBlob](http://stackoverflow.com/a/5100158/)
+* [Angular File Upload by nervgh](https://github.com/nervgh/angular-file-upload/)
---
+++ server.js
@@ -0,0 +1,22 @@
+var dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+var path = require("path");
+
+app.use(express.static(path.join(__dirname, '/')));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/s3creds',function(req,res){
+    s3 = {};
+    s3.region = process.env.REGION;
+    s3.accessKeyId = process.env.ACCESSKEYID;
+    s3.secretAccessKey = process.env.SECRETACCESSKEY;
+    s3.bucket = process.env.BUCKET;
+    res.json(s3); 
+});
+
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
