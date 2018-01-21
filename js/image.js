85207e40e35c2e0090f2653ac411d35baa269eb6
elishakay <kramer1346@gmail.com>
2018-01-21 20:18:43 +0200

    added function img.onload

--- js/image.js
+++ js/image.js
@@ -23,21 +23,28 @@
 					var canvas = document.createElement('canvas');
 					var ctx = canvas.getContext("2d");
 
-					reader.onload = function() {
-						img.src = reader.result;
-						scope.image.srcWidth = img.width;
-						scope.image.srcHeight = img.height;
-						scope.image.srcSize = file.size;
-						canvas.width = scope.image.srcWidth;
-						canvas.height = scope.image.srcHeight;
-						ctx.drawImage(img, 0, 0);
+					reader.onload = function(theFile) {
+						var img = new Image();
+						img.src = theFile.target.result;
 
-						scope.image.ratio = Math.min(controller.maxWidth / scope.image.srcWidth, controller.maxHeight / scope.image.srcHeight);
-						resample_hermite(canvas, scope.image.srcWidth, scope.image.srcHeight, Math.round(scope.image.srcWidth * scope.image.ratio), Math.round(scope.image.srcHeight * scope.image.ratio));
-						var dataURL = canvas.toDataURL("image/jpeg", 0.92);
-						var blob = dataURItoBlob(dataURL);
-						scope.image.resultSize = blob.size;
-						scope.image.compression = ($filter('number')((scope.image.srcSize - scope.image.resultSize) / scope.image.srcSize * 100, 2));
+						img.onload = function() {
+      						// access image size here 
+        					scope.image.srcWidth = img.width;
+							console.log('this is the source width',img.width);
+							scope.image.srcHeight = img.height;
+							scope.image.srcSize = file.size;
+							canvas.width = scope.image.srcWidth;
+							canvas.height = scope.image.srcHeight;
+							ctx.drawImage(img, 0, 0);
+
+							scope.image.ratio = Math.min(controller.maxWidth / scope.image.srcWidth, controller.maxHeight / scope.image.srcHeight);
+							resample_hermite(canvas, scope.image.srcWidth, scope.image.srcHeight, Math.round(scope.image.srcWidth * scope.image.ratio), Math.round(scope.image.srcHeight * scope.image.ratio));
+							var dataURL = canvas.toDataURL("image/jpeg", 0.92);
+							var blob = dataURItoBlob(dataURL);
+							scope.image.resultSize = blob.size;
+							scope.image.compression = ($filter('number')((scope.image.srcSize - scope.image.resultSize) / scope.image.srcSize * 100, 2));
+    					};
+
 
 						if (controller.isSimulation) {
 							scope.image.dataURL = dataURL;
